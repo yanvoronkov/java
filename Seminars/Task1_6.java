5c174498226dd378aed936fb3c69b43b02791d26
Yan Voronkov <sub.voronkov@gmail.com>
2023-04-22 21:35:18 +0300

    Добавлена задача Task1_6.java

--- Seminars/Task1_6.java
+++ Seminars/Task1_6.java
@@ -1,4 +1,14 @@
 package Seminars;
 
-public class test {
+import java.util.Scanner;
+
+public class Task1_6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int n = in.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
 }
