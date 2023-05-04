e6fa8d16629e17244539867b66d21136be59d384
Yan Voronkov <sub.voronkov@gmail.com>
2023-05-04 11:28:43 +0300

    Task198

--- Stepic/Task198.java
+++ Stepic/Task198.java
@@ -7,13 +7,20 @@ public class Task198 {
 
         Scanner sc = new Scanner(System.in);
         int res = 1;
+        boolean flag = false;
         for (String i = sc.next(); true; i = sc.next()) {
-            if (i.equals("СТОП")) break;
+            if (i.equalsIgnoreCase("СТОП")) break;
             int n = Integer.parseInt(i);
             if (n != 0) {
                 res *= n;
+                flag = true;
             }
         }
-        System.out.println(res);
+        if (flag){
+            System.out.println(res);
+        } else {
+            System.out.println("Не найдено");
+        }
+
     }
 }
