f4a41f0c16d0d93cb6a7178599cdb52916cc6ace
Yan Voronkov <sub.voronkov@gmail.com>
2023-05-11 17:14:49 +0300

    Task1156

---
+++ Stepic/Task1156.java
@@ -0,0 +1,37 @@
+package Stepic;
+
+import java.util.Scanner;
+
+public class Task1156 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean flag = false;
+        int[][] matrix = new int[n][n];
+        boolean isSimmetric = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    isSimmetric = false;
+                }
+            }
+        }
+        if (isSimmetric) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+
+
+
+    }
+}
+
+
