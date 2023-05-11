989391c4be78dac8d3ebf04a59287ffa7708a68f
Yan Voronkov <sub.voronkov@gmail.com>
2023-05-11 09:40:18 +0300

    Task1143

---
+++ Stepic/Task1143.java
@@ -0,0 +1,33 @@
+package Stepic;
+
+import java.util.Scanner;
+
+public class Task1143 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    matrix[i][j] = 1;
+                }
+                if (i > j) {
+                    matrix[i][j] = 2;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
