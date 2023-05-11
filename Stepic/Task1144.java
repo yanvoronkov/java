83c09e1c1bbbb46535e0cc5146641968e99b1553
Yan Voronkov <sub.voronkov@gmail.com>
2023-05-11 09:44:38 +0300

    Task1144

--- Stepic/Task1144.java
+++ Stepic/Task1144.java
@@ -20,7 +20,6 @@ public class Task1144 {
                 }
             }
         }
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(matrix[i][j] + " ");
