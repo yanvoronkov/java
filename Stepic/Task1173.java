914613b3add393ee1928d70fe8fcdc34c507bd49
Yan Voronkov <sub.voronkov@gmail.com>
2023-05-15 20:49:51 +0300

    Task1173

--- Stepic/Task1173.java
+++ Stepic/Task1173.java
@@ -2,27 +2,30 @@ package Stepic;
 
 import java.util.Scanner;
 
-public class Task1172 {
+public class Task1173 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        char letter = scanner.next().charAt(0);
-        int count = 0;
+
+        int countDigits = 0;
+        int countLetters = 0;
+
         for (int i = 0; i < str.length(); i++) {
-            if (str.toLowerCase().charAt(i) == Character.toLowerCase(letter)) {
-                count++;
+            if (Character.isDigit(str.charAt(i))) {
+                countDigits++;
+            } else if (Character.isLetter(str.charAt(i))) {
+                countLetters++;
             }
         }
-        System.out.println(count);
+        if (countDigits > countLetters) {
+            System.out.println("Digit");
+        } else if (countDigits < countLetters) {
+            System.out.println("Letter");
+        } else {
+            System.out.println("Equal");
+        }
 
-//        char [] arr = str.toCharArray();
-//        for (int i = 0; i < arr.length; i++) {
-//            if (arr[i] == 'f') {
-//                count++;
-//            }
-//        }
-//        System.out.println(count);
 
 
     }
