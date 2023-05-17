6d127a1de5c2fcd9ecbf3806be274517f1785acf
Yan Voronkov <sub.voronkov@gmail.com>
2023-05-17 15:35:37 +0300

    Task3_4

--- Seminars/Task3_4.java
+++ Seminars/Task3_4.java
@@ -2,18 +2,27 @@ package Seminars;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Task3_4 {
 	public static void main(String[] args) {
 
-		ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-		for (int i = 0; i < list.size(); i++){
-			if (list.get(i) % 2 == 0){
-                list.remove(i);
-                i--;
-            }
-		}
-		System.out.println(list);
+		ArrayList<Integer> list = new ArrayList<>(Arrays.asList(4, 31, 1, 4, 10, 6, 17, 8, 9, 15));
+		int maxValue = list.get(0);
+		int minValue = list.get(0);
+		double sum = 0;
+		double average = 0;
 
+		maxValue = Collections.max(list);
+		minValue = Collections.min(list);
+
+		for (Integer num : list) {
+            sum += num;
+        }
+
+		average = sum / list.size();
+		System.out.println("Min value: " + minValue);
+		System.out.println("Max value: " + maxValue);
+		System.out.println("Average: " + average);
 	}
 }
