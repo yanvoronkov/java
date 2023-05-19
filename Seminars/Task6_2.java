d7ffd86da7b5598868b6dc432c4c488cc4dcbca1
Yan Voronkov <sub.voronkov@gmail.com>
2023-05-19 15:14:44 +0300

    Task6_2

--- Seminars/Task6_2.java
+++ Seminars/Task6_2.java
@@ -2,25 +2,31 @@ package Seminars;
 
 import java.util.*;
 
-public class Task6_1 {
+public class Task6_2 {
     public static void main(String[] args) {
 
-
         /*
-        1. Создайте HashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
-        Распечатайте содержимое данного множества.
-        2. Создайте LinkedHashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
-        Распечатайте содержимое данного множества.
-        3. Создайте TreeSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
-        Распечатайте содержимое данного множества.
+        1. Напишите метод, который заполнит массив из 1000 элементов случайными цифрами от 0 до 24.
+        2. Создайте метод, в который передайте заполненный выше массив и с помощью Set вычислите процент
+        уникальных значений в данном массиве и верните его в виде числа с плавающей запятой.
+        Для вычисления процента используйте формулу:
+        процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
          */
-            Integer[] arr = new Integer[]{3, 1, 2, 2, 6, 4, 5, 3};
-            Set<Integer> set1= new HashSet<>(Arrays.asList(arr));
-            Set<Integer> set2= new LinkedHashSet<>(Arrays.asList(arr));
-            Set<Integer> set3= new TreeSet<>(Arrays.asList(arr));
-            System.out.println(set1);
-            System.out.println(set2);
-            System.out.println(set3);
-
-    }
+            Integer[] arr = createArray(0, 24, 100);
+//        System.out.println(Arrays.toString(arr));
+            double percent = unic(arr);
+            System.out.println("Процент уникальных значений: " + percent);
+        }
+        static double unic(Integer[] arr){
+            Set<Integer> set = new HashSet<>(Arrays.asList(arr));
+            double percent = set.size() * 100.0 / arr.length;
+            return percent;
+        }
+        static Integer[] createArray(int min, int max, int capacity){
+            Integer[] arr = new Integer[capacity];
+            for (int i = 0; i < capacity; i++) {
+                arr[i] = (int)(Math.random() * (max - min + 1) + min);
+            }
+            return arr;
+        }
 }
