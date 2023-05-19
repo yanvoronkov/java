160c0a7a7258c72568943054e14b9233c9cce2ff
Yan Voronkov <sub.voronkov@gmail.com>
2023-05-19 09:54:50 +0300

    Task5_2h

---
+++ Seminars/Task5_2h.java
@@ -0,0 +1,33 @@
+package Seminars;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Task5_2h {
+    public static void main(String[] args) {
+
+        /*
+Пусть дан список сотрудников: Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина,
+Анна Крутова, Иван Юрин, Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова,
+Мария Савина, Мария Рыкова, Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов.
+Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
+Отсортировать по убыванию популярности. Для сортировки использовать TreeMap.
+         */
+
+        List<String> names = List.of("Иван Иванов", "Светлана Петрова", "Кристина Белова", "Анна Мусина", "Анна Крутова", "Иван Юрин", "Петр Лыков", "Павел Чернов", "Петр Чернышов", "Мария Федорова", "Марина Светлова", "Мария Савина", "Мария Рыкова", "Марина Лугова", "Анна Владимирова", "Иван Мечников", "Петр Петин", "Иван Ежов");
+
+        Map<String, Integer> nameCounts = new HashMap<>();
+
+        for (String name : names) {
+            String[] parts = name.split(" ");
+            String firstName = parts[0];
+            nameCounts.put(firstName, nameCounts.getOrDefault(firstName, 0) + 1);
+        }
+
+        nameCounts.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+    }
+}
